25d7a3195f0248321e31a1d66b542f95dcf1fab4
Hyunsu Lim <karegoie@snu.ac.kr>
2026-02-26 13:01:31 +0900

    mutthrehs

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ struct Args {
     genes: PathBuf,
     #[arg(long)]
     output: PathBuf,
-    #[arg(long, default_value_t = 3)]
+    #[arg(long, default_value_t = 100)]
     mutation_threshold: u32,
     #[arg(long, default_value_t = 5)]
     grna_buffer: usize,
